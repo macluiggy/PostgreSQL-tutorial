a2d011f13ce909972dae57ab605da52bc0b16a50
luiggy macias <ferrinluiggy@gmail.com>
2022-03-25 09:56:49 -0500

    learning about exists operator

--- section 7/exists.sql
+++ section 7/exists.sql
@@ -1,2 +1,52 @@
 -- el operador EXISTS se utiliza para saber si existe uno o mas filas en una consulta
--- el contenido de la subconsulta no es importante
+-- el contenido de select de la subconsulta no es importante
+-- por esta razon la convencion comun al usar este operador es:
+SELECT 
+    column1
+FROM 
+    table_1
+WHERE 
+    EXISTS( SELECT 
+                1 
+            FROM 
+                table_2 
+            WHERE 
+                column_2 = table_1.column_1);
+
+select first_name, last_name
+from customer c
+where exists (
+  select 1
+  from payment p
+  where p.customer_id = c.customer_id
+  and amount > 11
+) 
+order by first_name, last_name;
+-- en este ejemplo, para cada cliente in la tabla customer, la subconsulta verifica la tabla paymente para encontrar se ese cliente hizo al menos un pago (p.customer_id = c.customer_id) y que el monto del pago sea mayor a 11 (amount > 11)
+
+-- not exists es igual que exists solo que este devulve false si existe una o mas filas en la subconsulta y true si no existe ninguna fila
+
+select first_name, last_name
+from customer c
+where not exists (
+  select 1
+  from payment p
+  where p.customer_id = c.customer_id
+  and amount > 11
+
+)
+order by first_name, last_name;
+-- esta cosulta devulve los resultados que no sean iguales a la constulta anterior
+
+-- si la subquery retorna null, exists devuelve true
+SELECT
+	first_name,
+	last_name
+FROM
+	customer
+WHERE
+	EXISTS( SELECT NULL )
+ORDER BY
+	first_name,
+	last_name;
+-- esta consulta devolvera todas las filas de la tabla customer ya que exists devuelve true para cada fila
